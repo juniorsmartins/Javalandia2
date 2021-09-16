258f1613619d07567647581cc4404d001955757a
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-09-16 05:48:22 -0400

    Estudo de Java

--- src/main/java/com/utfpr/avaliacoes/redes/atividadefinal/ICalculadora.java
+++ src/main/java/com/utfpr/avaliacoes/redes/atividadefinal/ICalculadora.java
@@ -1,6 +1,12 @@
 package com.utfpr.avaliacoes.redes.atividadefinal;
 
-public interface ICalculadora 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ICalculadora extends Remote
 {
-    
+    public long add(long a, long b) throws RemoteException;
+    public long sub(long a, long b) throws RemoteException;
+    public long mul(long a, long b) throws RemoteException;
+    public long div(long a, long b) throws RemoteException;
 }
