13f9c21f1d109f8b6525ae38d38fd4b56499b8d9
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-09-17 10:01:22 -0400

    Estudo de Java

---
+++ src/main/java/com/java/redesRMI/ex02/ClienteRMI.java
@@ -0,0 +1,25 @@
+package com.java.redesRMI.ex02;
+
+import com.utfpr2.Validadora;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class ClienteRMI 
+{
+    public static void main(String[] args) 
+    {
+        try
+        {
+            Registry registro = LocateRegistry.getRegistry("localhost", 1099);
+            Validadora stub = (Validadora) registro.lookup("metodosValidadores");
+            
+            System.out.println("O CPF 05342888876 está: " );
+            if(stub.validarCPF("05342888876"))
+                System.out.println("Correto!");
+            else
+                System.out.println("Incorreto!");
+            System.out.println("O número 10 é " + stub.maiorMenorZero(10));
+        }catch(Exception e)
+        {e.printStackTrace();}
+    }
+}
