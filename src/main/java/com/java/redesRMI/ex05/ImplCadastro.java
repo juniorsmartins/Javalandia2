a2c25c205585c18882d1d103df1410307d2406c4
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-09-18 07:06:47 -0400

    Estudo de Java

---
+++ src/main/java/com/java/redesRMI/ex05/ImplCadastro.java
@@ -0,0 +1,36 @@
+package com.java.redesRMI.ex05;
+
+import com.utfpr5.interfaces.iCadastro;
+import com.utfpr5.interfaces.iOlaMundo;
+import java.io.Serializable;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+
+public class ImplCadastro implements iCadastro, Serializable
+{
+    @Override
+    public boolean cadastrar(String[] dados) throws RemoteException 
+    {System.out.println("Dados: " + dados); return true;}
+
+    @Override
+    public String consultarID(int id) throws RemoteException 
+    {System.out.println("ID: " + id); return "Nome";}
+
+    @Override
+    public String consultarNome(String nome) throws RemoteException 
+    {
+        String resposta = nome;
+        try
+        {
+            iOlaMundo stub = (iOlaMundo) Naming.lookup("rmi://127.0.0.1/servicoOla");
+            resposta = nome + " - " + stub.olaMundo();
+            System.out.println("Servidor OlaMundo: " + stub.olaMundo());
+        }catch(Exception e)
+        {e.printStackTrace();}
+        return resposta;
+    }
+
+    @Override
+    public boolean removerID(String id) throws RemoteException 
+    {System.out.println("ID: " + id); return true;}    
+}
---
+++ src/main/java/com/java/redesRMI/ex05/ServidorServico.java
@@ -0,0 +1,42 @@
+package com.java.redesRMI.ex05;
+
+import com.utfpr5.impl.ImplOlaMundo;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ServidorServico extends UnicastRemoteObject
+{
+    public ServidorServico() throws RemoteException
+    {super();}
+    
+    public static void main(String[] args) 
+    {
+        try 
+        {
+            new ServidorServico().iniciar();
+        } catch (RemoteException ex) 
+        {
+            Logger.getLogger(ServidorServico.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+    }
+    
+    public void iniciar()
+    {    
+        try 
+        {
+            Registry conexao = LocateRegistry.getRegistry(1099);
+            Naming.rebind("servicoOla", new ImplOlaMundo());
+            System.out.println("Servidor OlaMundo iniciado...");
+        } catch (RemoteException ex) {
+            Logger.getLogger(ServidorServico.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(ServidorServico.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }    
+}
