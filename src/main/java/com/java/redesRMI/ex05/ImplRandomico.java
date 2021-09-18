d045afcd9a86b123e64fe1fb7f8237b0951e02d4
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-09-18 07:08:13 -0400

    Estudo de Java

---
+++ src/main/java/com/java/redesRMI/ex05/ImplRandomico.java
@@ -0,0 +1,29 @@
+package com.java.redesRMI.ex05;
+
+import com.utfpr5.interfaces.iRandomico;
+import java.io.Serializable;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Random;
+
+public class ImplRandomico extends UnicastRemoteObject implements iRandomico, Serializable
+{
+    private Random random = new Random();
+    
+    public ImplRandomico() throws RemoteException
+    {super();}
+    
+    @Override
+    public int retornaValorRandomico() throws RemoteException 
+    {
+        int valor = random.nextInt();
+        return valor;
+    }
+
+    @Override
+    public int retornaValorRandomicoIntervalo(int a, int b) throws RemoteException 
+    {
+        int valor = a + random.nextInt(a + b);
+        return valor;
+    }
+}
