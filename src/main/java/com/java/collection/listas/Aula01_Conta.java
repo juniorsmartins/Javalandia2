d685dafcfcbf4345f6dad397870ab86dfa8c9848
JuniorMartins <juniorsoaresmartins@gmail.com>
2021-09-24 14:34:56 -0300

    Estudo prático de Java.

---
+++ src/main/java/com/java/collection/listas/Aula01_Conta.java
@@ -0,0 +1,39 @@
+package com.java.collection.listas;
+
+import java.util.Objects;
+
+public abstract class Aula01_Conta
+{
+    private int agencia;
+    private int numero;
+
+    public Aula01_Conta(int agencia, int numero)
+    {
+        this.agencia = agencia;
+        this.numero = numero;
+    }
+
+    public int getNumero()
+    {return numero;}
+    public void setNumero(int numero)
+    {this.numero = numero;}
+    public int getAgencia()
+    {return agencia;}
+    public void setAgencia(int agencia)
+    {this.agencia = agencia;}
+
+    @Override
+    public boolean equals(Object o)
+    {
+        Aula01_Conta conta = (Aula01_Conta) o;
+        if(this.getAgencia() != conta.getAgencia())
+        {return false;}
+        if(this.getNumero() != conta.getNumero())
+        {return false;}
+        return true;
+    }
+
+    @Override
+    public int hashCode()
+    {return Objects.hash(getAgencia(), getNumero());}
+}
---
+++ src/main/java/com/java/collection/listas/Aula01_Main.java
@@ -0,0 +1,4 @@
+package com.java.collection.listas;
+
+public class Aula01_Main {
+}
