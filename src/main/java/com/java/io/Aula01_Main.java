f927749263fcfbdce0fb13937c37c127dba4eee0
JuniorMartins <juniorsoaresmartins@gmail.com>
2021-09-25 08:43:07 -0300

    Estudo prático de Java.

--- src/main/java/com/java/io/Aula01_Main.java
+++ src/main/java/com/java/io/Aula01_Main.java
@@ -1,20 +1,20 @@
 package com.java.io;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Aula01_Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        try(FileInputStream fis = new FileInputStream("lorem.txt");)
-        {
+        FileInputStream fis = new FileInputStream("lorem.txt");
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
+        String linha = br.readLine();
+        System.out.println("\n" + linha);
 
-        }catch(IOException ioe)
-        {
-            System.err.println("\nExceçao!\n");
-            ioe.printStackTrace();
-        }
-
+        br.close();
     }
 }
