d188ce0aed22dd3941983eb3912c5e392b45dbb6
JuniorMartins <juniorsoaresmartins@gmail.com>
2021-09-28 11:42:20 -0300

    Estudo prático de Java.

--- src/main/java/com/java/junit/Aula01_CalculadoraTest.java
+++ src/main/java/com/java/junit/Aula01_CalculadoraTest.java
@@ -1,6 +1,16 @@
 package com.java.junit;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class Aula01_CalculadoraTest
 {
+    @Test
+    public void deveriaSomarDoisNumerosPositivos()
+    {
+        Aula01_Calculadora calc1 = new Aula01_Calculadora();
+        int soma1 = calc1.somar(3,7);
 
+        Assertions.assertEquals(10, soma1);
+    }
 }
