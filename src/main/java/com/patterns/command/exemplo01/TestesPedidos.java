84aa37030360fed117aade70e27c218652d28099
JuniorMartins <juniorsoaresmartins@gmail.com>
2021-11-06 08:52:29 -0400

    Estudo prático de Java.

---
+++ src/main/java/com/patterns/command/exemplo01/TestesPedidos.java
@@ -0,0 +1,20 @@
+package com.patterns.command.exemplo01;
+
+public class TestesPedidos
+{
+    public static void main(String[] args)
+    {
+        Orcamento orcamento = new Orcamento();
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
