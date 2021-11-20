6cd7836ca393c04da6254c5273be83f9100c26a2
juniorsmartins <juniorsoaresmartins@gmail.com>
2021-11-20 14:32:27 -0400

    Estudo prático de Java.

--- src/main/java/com/java/data/Aula07_LocalDate_Formatter.java
+++ src/main/java/com/java/data/Aula07_LocalDate_Formatter.java
@@ -3,6 +3,7 @@ package com.java.data;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
 public class Aula07_LocalDate_Formatter
 {
@@ -10,6 +11,8 @@ public class Aula07_LocalDate_Formatter
     {
         executarDatas();
         executarDatasHoras();
+        executarDatasHorasSemPrecisarDefinirFormato();
+        executarDataSemPrecisarDefinirFormato();
     }
 
     public static void executarDatas()
@@ -31,12 +34,26 @@ public class Aula07_LocalDate_Formatter
         System.out.println(agora.format(formatador));
     }
 
+    public static void executarDatasHorasSemPrecisarDefinirFormato()
+    {
+        System.out.println("------------------------------");
+        // gerar data e hora
+        LocalDateTime dataHora1 = LocalDateTime.now();
 
+        // formatar sem definir formato - Fazer com formato padrão (Short ou Medium)
+        DateTimeFormatter formatadorPadrao1 = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
+        System.out.println(dataHora1.format(formatadorPadrao1));
+    }
 
-
-
-
-
+    public static void executarDataSemPrecisarDefinirFormato()
+    {
+        System.out.println("--------------------------------");
+        // gerar data
+        LocalDate data2 = LocalDate.now();
+        // formatar com formato padrão (Long ou Full)
+        DateTimeFormatter formatadorPadrao2 = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL);
+        System.out.println(data2.format(formatadorPadrao2));
+    }
 }
 
 
