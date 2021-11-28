bc2134cf7b46c6d0ec1e46b90d5fc843487c6a82
JuniorMartins <juniorsoaresmartins@gmail.com>
2021-11-28 15:30:25 -0400

    Estudo prático de Java.

---
+++ src/main/java/com/desafios/bootcampcognizant/d3/Desenho.java
@@ -0,0 +1,15 @@
+package com.desafios.bootcampcognizant.d3;
+
+public class Desenho
+{
+    public static void main(String[] args)
+    {
+        for(int i = 0; i < 39; i++)
+            System.out.print("-");
+        System.out.println("");
+        for(int y = 0; y < 5; y++)
+            System.out.println("|                                     |");
+        for(int i = 0; i < 39; i++)
+            System.out.print("-");
+    }
+}
