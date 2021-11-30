a5ad8f5a7ba9b8cc12fb44d16f13e272bb642fdd
JuniorMartins <juniorsoaresmartins@gmail.com>
2021-11-30 17:35:48 -0400

    Estudo prático de Java.

---
+++ src/main/java/com/estruturadedados/arvore/aula01/model/ObjArvore.java
@@ -0,0 +1,10 @@
+package com.estruturadedados.arvore.aula01.model;
+
+public abstract class ObjArvore<T> implements Comparable<T>
+{
+    public abstract boolean equals(Object o);
+    public abstract int hashCode();
+    public abstract int compareTo(T outro);
+    public abstract String toString();
+
+}
